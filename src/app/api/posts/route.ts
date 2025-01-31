94ee3ab531a6c92695430893fd72bdca7c1c50f0
mykhailo.bakhmut <mykhailo.bakhmut.ca@gmail.com>
2025-01-31 13:23:26 -0500

    auth_3

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,5 +1,6 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
+import { auth } from "../../../../auth.config";
 
 export async function GET() {
   try {
@@ -11,6 +12,7 @@ export async function GET() {
 }
 
 export async function POST(request: Request) {
+  const session = await auth();
   const { searchParams } = new URL(request.url);
   const id = searchParams.get("id");
   const title = searchParams.get("title");
@@ -19,6 +21,12 @@ export async function POST(request: Request) {
   const author = searchParams.get("author");
 
   try {
+    if (!session) {
+      return NextResponse.json(
+        { message: "user not authenticated" },
+        { status: 401 }
+      );
+    }
     // SQL query to insert a new post
     await sql`INSERT INTO posts (id, author, title, content, date) VALUES (${id}, ${author}, ${title}, ${content}, ${date});`;
     return NextResponse.json(
